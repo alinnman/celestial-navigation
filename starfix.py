eec1e3b02582fb3f2865aa98a87884108a968a83
August Linnman <august@linnman.net>
2024-05-14 00:35:20 +0200

    Cleaned up code

--- starfix.py
+++ starfix.py
@@ -62,7 +62,6 @@ def toLonLat (vec):
     vec = normalizeVect (vec)
     
     theta = atan2 (vec[1],vec[0])
-    #print ("BLALA = " + str(vec[2]))
     phi = acos (vec[2])
     LON = radToDeg (theta)
     LAT = 90-radToDeg (phi) 
@@ -77,7 +76,6 @@ def toRectangular (LON, LAT):
     aVec.append (cos (theta) * sin (phi))
     aVec.append (sin (theta) * sin (phi))
     aVec.append (cos (phi))
-    #print ("TJOSAN = " + str(aVec))
     aVec = normalizeVect (aVec)
     return aVec
     
@@ -127,40 +125,36 @@ class starFix :
                   sha_diff_degrees = 0, \
                   sha_diff_minutes = 0):
         self.object_name          = object_name
-        self.time_hour            = time_hour           # B7
-        self.time_minute          = time_minute         # B8
-        self.time_second          = time_second         # B9
-        self.gha_time_0_degrees   = gha_time_0_degrees  # B12
-        self.gha_time_0_minutes   = gha_time_0_minutes  # B13
-        self.gha_time_1_degrees   = gha_time_1_degrees  # B14
-        self.gha_time_1_minutes   = gha_time_1_minutes  # B15
-        self.decl_time_0_degrees  = decl_time_0_degrees # D12
-        self.decl_time_0_minutes  = decl_time_0_minutes # D13 
-        self.decl_time_1_degrees  = decl_time_1_degrees # D14 
-        self.decl_time_1_minutes  = decl_time_1_minutes # D15
+        self.time_hour            = time_hour            
+        self.time_minute          = time_minute          
+        self.time_second          = time_second          
+        self.gha_time_0_degrees   = gha_time_0_degrees  
+        self.gha_time_0_minutes   = gha_time_0_minutes  
+        self.gha_time_1_degrees   = gha_time_1_degrees  
+        self.gha_time_1_minutes   = gha_time_1_minutes  
+        self.decl_time_0_degrees  = decl_time_0_degrees 
+        self.decl_time_0_minutes  = decl_time_0_minutes  
+        self.decl_time_1_degrees  = decl_time_1_degrees  
+        self.decl_time_1_minutes  = decl_time_1_minutes 
         self.measured_alt_degrees = measured_alt_degrees
         self.measured_alt_minutes = measured_alt_minutes
         self.measured_alt_seconds = measured_alt_seconds
-        self.sha_diff_degrees     = sha_diff_degrees    # B10
-        self.sha_diff_minutes     = sha_diff_minutes    # B11
+        self.sha_diff_degrees     = sha_diff_degrees    
+        self.sha_diff_minutes     = sha_diff_minutes    
         assert (self.object_name != "Sun" or (self.sha_diff_degrees == 0 and self.sha_diff_minutes == 0))
         
         self.GP_lon, self.GP_lat = self.__calculateGP ()
-        #print (str(self.GP_lon), "", str(self.GP_lat))
     
     def __calculateGP (self):
-        # -((B12+B10)+((B14+B10)-(B12+B10))*C9+((B13+B11)/60)+(((B15+B11)-(B13+B11))/60)*C9)
-        # -((B12+B10)+((B14-B12          ))*C9+((B13+B11)/60)+(((B15-B13)/60))*C9)
         
         minSecContribution = self.time_minute/60 + self.time_second/3600
-        #print (minSecContribution)
         
         resultLON = - \
         ((self.gha_time_0_degrees + self.sha_diff_degrees) + \
         ((self.gha_time_1_degrees - self.gha_time_0_degrees))*minSecContribution + \
         ((self.gha_time_0_minutes + self.sha_diff_minutes)/60) + \
         (((self.gha_time_1_minutes - self.gha_time_0_minutes)/60))*minSecContribution)
-        # D12+(D14-D12)*C9+(D13/60)+((D15-D13)/60)*C9
+ 
         resultLAT = \
         self.decl_time_0_degrees + (self.decl_time_1_degrees - self.decl_time_0_degrees)*minSecContribution + \
         self.decl_time_0_minutes/60 + ((self.decl_time_1_minutes - self.decl_time_0_minutes)/60)*minSecContribution
@@ -185,20 +179,12 @@ class starFixPair:
         self.sf2 = sf2
 
     def getIntersections (self):
-
-        #print ("FOO1-LON = " + str(self.sf1.GP_lon)+ "; FOO1-LAT = " + str(self.sf1.GP_lat))               
         aVec = toRectangular (self.sf1.GP_lon, self.sf1.GP_lat)
-        #print ("AVEC = " + str(aVec))
         LON1, LAT1 = toLonLat (aVec)
-        #print ("AVEC: LON1 = " + str(LON1)+ "; LAT1 = " + str(LAT1))       
-        
-        #print ("FOO2-LON = " + str(self.sf2.GP_lon)+ "; FOO2-LAT = " + str(self.sf2.GP_lat)) 
-        bVec = toRectangular (self.sf2.GP_lon, self.sf2.GP_lat)
-        #print ("BVEC = " + str(bVec))        
+     
+        bVec = toRectangular (self.sf2.GP_lon, self.sf2.GP_lat)       
         LON1, LAT1 = toLonLat (bVec)
-        #print ("BVEC: LON1 = " + str(LON1)+ "; LAT1 = " + str(LAT1))               
-        
-        
+              
         abCross = crossProduct (aVec, bVec)
         abCross = normalizeVect (abCross)
 
@@ -210,16 +196,9 @@ class starFixPair:
         q = normalizeVect (p4)
 
         qLon, qLat = toLonLat (q)
-        #print ("LON = " + str(qLon)+ "; LAT = " + str(qLat))
-        # LON = -91.44736977201018; LAT = 24.808471771121802
-        # 24.808471771121802,-91.44736977201018
 
         rho = acos (cos (degToRad(self.sf1.getAngle())) / (dotProduct (aVec, q)))
         rotAxis = normalizeVect(crossProduct (crossProduct (aVec, bVec), q))
-        #print ("ROTAXIS = " + str(rotAxis))
-        #print (q)
-        #print (lengthOfVect(q))
-        #print ("RHO = " + str(radToDeg(rho)))
         
         int1 = normalizeVect(rotateVector (q, rotAxis, rho))
         int2 = normalizeVect(rotateVector (q, rotAxis, -rho))
@@ -236,56 +215,38 @@ class starFixCollection:
             return intersections
         else:
             nrOfFixes = len (self.sfList)
-            #print ("nrOfFixes = " + str(nrOfFixes))
             coords = []
             for i in range (nrOfFixes):
                 for j in range (i, nrOfFixes):
                     if i != j:
-                        #print ("i = " + str(i) + "; j = " + str(j))
                         p = starFixPair (self.sfList [i], self.sfList [j])
                         pInt = p.getIntersections ()
-                        #print (pInt)
                         coords.append (pInt[0])
                         coords.append (pInt[1])                        
-            #print (len(coords))
-            #print (coords)
             nrOfCoords = len (coords)
             dists = dict ()
             for i in range (nrOfCoords):
                 for j in range (i, nrOfCoords):
                     if i != j:
                         dist = distanceBetweenPoints (coords[i], coords[j])
-                        #print (dist)
                         dists [i,j] = dist
-            #print (dists)
             sortedDists = dict(sorted(dists.items(), key=lambda item: item[1]))
-            #print (sortedDists)
             nrOfSortedDists = len (sortedDists)
             chosenPoints = set ()
             for sd in sortedDists:
-                #print (sd) 
                 theDistance = sortedDists [sd]
-                #print (theDistance)
                 if (theDistance < limit): 
                     chosenPoints.add (sd[0])
                     chosenPoints.add (sd[1])
-            #print (chosenPoints) 
             nrOfChosenPoints = len (chosenPoints)
             summationVec = [0,0,0]
             for cp in chosenPoints: 
-                #print (cp) 
                 selectedCoord = coords [cp]
-                #print (selectedCoord) 
                 rectVec = toRectangular (selectedCoord[0], selectedCoord[1])
-                #print (rectVec) 
                 summationVec = addVecs (summationVec, multScalarVect (1/nrOfChosenPoints, rectVec))
             summationVec = normalizeVect (summationVec)
             retLON, retLAT = toLonLat (summationVec)
             return retLON, retLAT
-            
-            #nrOfDists = len (sortedDists)
-            
-        
         
 a = starFix (date                 = "2024-05-05", \
               object_name          = "Sun", \
@@ -305,7 +266,7 @@ a = starFix (date                 = "2024-05-05", \
               measured_alt_seconds = 1.8 \
               )
               
-#print (a.getRadius())
+# Our starfix data
 
 b = starFix (date                 = "2024-05-05", \
               object_name          = "Sun", \
@@ -345,16 +306,6 @@ c = starFix (date                 = "2024-05-06", \
               sha_diff_minutes     = 33.4 \
               )
 
-#print (b.getRadius())
-
-#starFixPair = starFixPair (a, b)
-#intersection1, intersection2 = starFixPair.getIntersections ()
-#print (intersection1)
-#print (intersection2)
-#dist = distanceBetweenPoints ((0,0), (0,90))
-#print (dist)
-# 41.764874280342816, -87.71719349718802
-
 collection = starFixCollection ([a, b])
 intersections = collection.getIntersections ()
 print (intersections)
@@ -362,6 +313,5 @@ print (intersections)
 collection = starFixCollection ([a, b, c])
 intersections = collection.getIntersections ()
 print (intersections)
-# 41.874156346243865, -87.67180300975592
 
 
