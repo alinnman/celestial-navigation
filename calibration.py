9a8141758e25692196b857ece4d9d9a0954db287
August Linnman <august@linnman.net>
2024-06-15 23:54:21 +0200

    More work

--- calibration.py
+++ calibration.py
@@ -1,7 +1,7 @@
 from starfix import angleBetweenPoints, getDMS, LatLon
 
-berget = LatLon (59.31994768792838, 18.050803976905534)
+mountain = LatLon (59.31994768792838, 18.050803976905534)
 scienceTower = LatLon (59.401619602468514, 17.9463822774136)
-johannes = LatLon (59.33945487100022, 18.06470993954498)
+riddarholmen = LatLon (59.32481913898507, 18.064878270190704)
 
-print ("Angle is " + str(getDMS(angleBetweenPoints(berget, scienceTower, johannes))))
+print ("Angle is " + str(getDMS(angleBetweenPoints(mountain, scienceTower, riddarholmen))))
