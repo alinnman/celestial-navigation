a299ef398adf0ebb2485658f7841094a1c16ccc5
August Linnman <august@linnman.net>
2024-10-22 22:35:47 +0200

    Added a new launch script

---
+++ launch.stat.1.diag.py
@@ -0,0 +1,7 @@
+''' Lauch Jupyter Notebook
+    © August Linnman, 2024, email: august@linnman.net
+    MIT License (see LICENSE file)
+'''
+
+from os import system
+system ("jupyter notebook notebook.stat.1.diag.ipynb")
